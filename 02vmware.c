63dcb2581fd6ef714276cdf7d1d0541b9c9a3b5c
PeterZhuo420 <mr.zhuo420@outlook.com>
2020-09-14 17:34:27 +0800

    修改02

--- 02vmware.c
+++ 02vmware.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 
 int main(int argc, char* argv[]) {
-	printf("hello gitHub, from wsl.\n");
-	printf("hello gitHub, from wsl again.\n");
-	printf("hello gitHub, from wsl again.\n");
+	printf("hello gitHub, from vmware.\n");
 	return 0;
 }

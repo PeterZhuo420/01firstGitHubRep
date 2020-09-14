9c5eeb7f6863e51127200577c9ed778bed20ff90
PeterZhuo420 <58561734+PeterZhuo420@users.noreply.github.com>
2020-09-14 18:04:53 +0800

    Update 01wsl.c

--- 01wsl.c
+++ 01wsl.c
@@ -5,5 +5,6 @@ int main(int argc, char* argv[]) {
 	printf("hello gitHub, from wsl again.\n");
 	printf("hello gitHub, from wsl again.\n");
 	printf("hello gitHub, from branch develop.\n");
+	printf("hello gitHub, from branch develop111111111.\n");
 	return 0;
 }

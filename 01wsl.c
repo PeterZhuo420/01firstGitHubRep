add511ad08f0d18858e4b291e114d429355fa4ee
PeterZhuo420 <mr.zhuo420@outlook.com>
2020-09-14 17:50:06 +0800

    add develop info

--- 01wsl.c
+++ 01wsl.c
@@ -4,5 +4,6 @@ int main(int argc, char* argv[]) {
 	printf("hello gitHub, from wsl.\n");
 	printf("hello gitHub, from wsl again.\n");
 	printf("hello gitHub, from wsl again.\n");
+	printf("hello gitHub, from branch develop.\n");
 	return 0;
 }

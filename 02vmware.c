ca341c21b036a86507ae936b1a5d3e9939a53397
PeterZhuo420 <mr.zhuo420@outlook.com>
2020-09-14 05:21:34 -0400

    add 02vmware.c

---
+++ 02vmware.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("hello gitHub, from wsl.\n");
+	printf("hello gitHub, from wsl again.\n");
+	printf("hello gitHub, from wsl again.\n");
+	return 0;
+}
